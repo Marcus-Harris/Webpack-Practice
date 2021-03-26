4e98e1d8bdc80b51d451810a22e303e6e943d7b1
Marcus Harris <79154965+Marcus-Harris@users.noreply.github.com>
2021-03-26 15:52:30 -0400

    Add files via upload

---
+++ src/Components/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div>
+      <h1>Webpack + React setup (I actually got this thing to work)!!!</h1>
+    </div>
+  );
+}
